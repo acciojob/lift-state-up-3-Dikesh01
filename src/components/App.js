0f9b5da74eef294340fa841b68569be63b9785cc
kabir33559@gmail.com <Dikesh01>
2023-08-16 12:01:51 +0530

    commiting

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,31 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [option, setOption] = useState('')
+
+  function handle1(){
+    setOption("Option 1")
+  }
+
+  function handle2(){
+    setOption("Option 2")
+  }
   return (
-    <div>
+    <div className="parent">
         {/* Do not remove the main div */}
+          <h1>Parent Component</h1>
+          <div className="child1">
+            <h2>Child Component 1</h2>
+            <button onClick={handle1}>Option 1</button>
+          </div>
+          <div className="child2">
+            <h2>Child Component 2</h2>
+            <button onClick={handle2}>Option 2</button>
+          </div>
+
+          <p>Selected Option : {option}</p>
     </div>
   )
 }
